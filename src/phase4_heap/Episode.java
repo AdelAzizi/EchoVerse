edc5fb36536394663be41a6ba213d1c1914492d6
adel azizi <adel2478azizi@gmail.com>
2026-01-22 00:53:08 +0330

    ساخت کلاس اپیزود

---
+++ src/phase4_heap/Episode.java
@@ -0,0 +1,12 @@
+package phase4_heap;
+
+public class Episode {
+
+    String id;
+    int priority;
+
+    public Episode(String id, int priority) {
+        this.id = id;
+        this.priority = priority;
+    }
+}
